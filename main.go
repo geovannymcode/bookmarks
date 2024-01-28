706a7b57ec84620f2930ddb51be525606fa7aa5c
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-01-28 00:10:24 -0500

    settings with format json

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Geovanny0401/bookmarks/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg, err := config.GetConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := gin.Default()
 	r.GET("/hello", hello)
-	log.Fatal(r.Run(":8080"))
+	log.Fatal(r.Run(fmt.Sprintf(":%d", cfg.ServerPort)))
 }
 
 func hello(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello Go!",
+		"message": "Hello World",
 	})
 }
